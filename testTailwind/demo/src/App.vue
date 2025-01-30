e6fe9840338b8af2a9d93421f8990ede91400ef4
Jiang CJ <2679631748@qq.com>
2025-01-30 13:39:51 +0800

    tailwindcss 测试

---
+++ testTailwind/demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- header.bg-blue-500.text-white.p-4 -->
+    <header class="bg-blue-500 text-white p-4">
+      <h1 class="text-xl font-bold">我的vlog</h1>
+    </header>
+    <main class="flex flex-grow  bg-red-500">
+      <aside class="w-1/4 bg-gray-200 p-4">
+        <nav>
+          <ul>
+            <li>链接1</li>
+            <li>链接2</li>
+            <li>链接3</li>
+          </ul>
+        </nav>
+      </aside>
+      <section class="w-3/4 p-4"> 
+        <p>这是主要的内容</p>
+      </section>
+    </main>
+    <footer class="bg-blue-500 text-white p-4">
+      <p>我的网站</p>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
