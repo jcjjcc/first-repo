1a02fb8ccb13ae86feff199e30128abef8ab3921
Jiang CJ <2679631748@qq.com>
2025-02-12 18:36:34 +0800

    作用域

---
+++ js/scope/10.js
@@ -0,0 +1,14 @@
+// var a = 10
+// if (true) {
+//     a = 20
+//     console.log(a) 
+// }
+// console.log(a) 
+
+var b = 10
+function myFunc() {
+     b = 20
+    console.log(b)
+}
+myFunc()
+console.log(b) 
